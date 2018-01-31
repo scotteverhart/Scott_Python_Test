a53761bb9f66e78a56dfc5f3064726a1b20277db
cseverhart <scott.everhart1@gmail.com>
2018-01-30 21:40:52 -0500

    Decoupled two methods and refactored.

--- Test_Module/myProgram.py
+++ Test_Module/myProgram.py
@@ -3,39 +3,49 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+url = 'https://blog.docker.com/category/engineering/'
 
-def main():
-    """Main entry point for the script."""
+blog_list = []
+response_date = []
 
-    url = 'https://blog.docker.com/category/engineering/'
 
-    response = requests.get(url)
-
-    #print response.text
-
-    blog_list = []
-
-    response_data = BeautifulSoup(response.text, "html.parser")
-
+def verify_url_response_data(response_data):
     if response_data.__len__() == 0:
         print "no data returned"
         exit(1)
 
-    for each_title in response_data.find_all('entry-title'):
-        print each_title.text
-        #blog_title = each_title.text
-        #if blog_title != 'X':
-        #    blog_list.append((blog_title.text))
 
-    #for blog_data in blog_list:
-    #    print blog_data.text
+def verify_response_is_valid(response):
+    if not response:
+        exit(2)
 
-    print 'End main'
+
+def main():
+    """Main entry point for the script."""
+
+    response = requests.get(url)
+
+    verify_response_is_valid(response)
+
+    response_data = BeautifulSoup(response.text, "html.parser")
+
+    verify_url_response_data(response_data)
+
+    for each_title in response_data.find_all(lambda tag: tag.name == 'h2'
+                                                         and tag.get('class') == ['entry-title']):
+        blog_title = each_title.text
+        if blog_title != 'X':
+            blog_list.append(blog_title)
+
+    print
+
+    for blog_data in blog_list:
+        print "blog_data-" + str(blog_list.index(blog_data)) + ": " + blog_data
+
+    print
+
     pass
 
 
 if __name__ == '__main__':
-    print
-    print 'starting program processing'
-    print
-    sys.exit(main())
+   sys.exit(main())
---
+++ Test_Module/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scott_Python_Test',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='cseverhart',
+    author_email='scott.everhart1@gmail.com',
+    description='This is my test python project'
+)
